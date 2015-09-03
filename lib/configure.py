cba2015b0a6f7652e39ee561bd9094bdd0f1eae1
kedos <shaked.dev@gmail.com>
2015-09-03 16:06:51 +0300

    Add run server script.

--- lib/configure.py
+++ lib/configure.py
@@ -6,6 +6,7 @@ import os
 def configure_app(app, db, login_service=None):
   # Choose configuration environment.
   config_name = os.environ.get("SERVER_ENV", "development")
+  print("Config Name: " + config_name)
   app.config.from_pyfile(
       os.path.join(os.getcwd(), "config", "%s.py" % config_name))
 
