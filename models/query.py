b327229c1dce5b002188bbe45855af8a836aff0c
RonnySivan <ronny.sivan1@gmail.com>
2015-09-03 16:31:06 +0300

    updated

--- models/query.py
+++ models/query.py
@@ -1,18 +1,28 @@
 __author__ = 'Ronny'
 
 class Query (){
+    # check if there's a friend in the location of my new wish
+    # pre - a new wish in the users list
+    # returns - list of user_id of the friends in the new wish's location
 
-    def getWishList (user):
-        return user.wishList
+    def addToWishList(user,WishItem):
+        relevantFriends = []
+        for friend in user.friendList:
+            if friend.location == WishItem.location:
+                relevantFriends.append(friend.user_id)
 
+
+
+
+    # check if there's a friend's wish in the location the user's traveling to
+    # returns - list of user_id of the friends that needs something from the users' location
 
     def travelToLocation (user,location):
         relevantFriends = []
         for friend in user.friendList:
             for wish in friend.wishList:
-                if wish.location == location:
-                    relevantFriends.append(friend.friend_id)
+                if wish.loaction == location:
+                    relevantFriends.append(friend.user_id)
 
         return relevantFriends
-
 }
--- models/user.py
+++ models/user.py
@@ -3,23 +3,24 @@ __author__ = 'shaked'
 from mongoengine import *
 from lib.id_generator import randomIdGenerator
 from wish_item import WishItem
-from friend_item import FriendItem
 from stringfield import StringField
 from djangotoolbox.fields import EmbeddedDocumentField, ListField
 
 class User(Document):
 
   user_id = StringField(default=randomIdGenerator("U"))
-  fb_id = StringField()
-  location = StringField()
+  fb_id = StringField() #/will get from login
+  cuurentLocation = StringField() #/will get from login
+  address = StringField() #/will get from login
   wishList = ListField(EmbeddedDocumentField(WishItem))
-  friendList = ListField(EmbeddedDocumentField(FriendItem))
+  friendList = ListField() #/will get from login
 
   def toMinimalJson(self):
     return {
       "userId": self.user_id,
       "FacebookId" : self.fb_id,
-      "Location" : self.location,
+      "Location" : self.currentLocation,
+      "address" : self.address,
       "wishList": [item.toMinimalJson for item in self.wishList],
       "friendList": [item.toMinimalJson for item in self.friendList]
     }
