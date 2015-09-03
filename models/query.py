4efb7927ae2dbec2e5029cd994f7bd291f8b8edd
RonnySivan <ronny.sivan1@gmail.com>
2015-09-03 15:41:12 +0300

    Add the traveling method

--- models/query.py
+++ models/query.py
@@ -2,5 +2,17 @@ __author__ = 'Ronny'
 
 class Query (){
 
-  def
+    def getWishList (user):
+        return user.wishList
+
+
+    def travelToLocation (user,location):
+        relevantFriends = []
+        for friend in user.friendList:
+            for wish in friend.wishList:
+                if wish.location == location:
+                    relevantFriends.append(friend.friend_id)
+
+        return relevantFriends
+
 }
--- models/user.py
+++ models/user.py
@@ -4,21 +4,24 @@ from mongoengine import *
 from lib.id_generator import randomIdGenerator
 from wish_item import WishItem
 from friend_item import FriendItem
+from stringfield import StringField
+from djangotoolbox.fields import EmbeddedDocumentField, ListField
 
 class User(Document):
 
   user_id = StringField(default=randomIdGenerator("U"))
   fb_id = StringField()
+  location = StringField()
   wishList = ListField(EmbeddedDocumentField(WishItem))
   friendList = ListField(EmbeddedDocumentField(FriendItem))
-
 
   def toMinimalJson(self):
     return {
       "userId": self.user_id,
       "FacebookId" : self.fb_id,
-      "wishList": [item.toMinimalJson() for item in self.wishList],
-      "friendList": [item.toMinimalJson() for item in self.friendList]
+      "Location" : self.location,
+      "wishList": [item.toMinimalJson for item in self.wishList],
+      "friendList": [item.toMinimalJson for item in self.friendList]
     }
 
   def toFullJson(self):
